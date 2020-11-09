e580189320fda0a53bd7f23bd1dc4814c45a3bc4
anikett-rpx <anikett_c@rpxcorp.com>
2020-11-09 23:17:33 +0530

    [9] Multiple Reducers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM"
 
 
 //Action is an object with a type property. And action creator is a function
@@ -13,13 +14,21 @@ function bycake() {
     }
 }
 
+function buyicecream() {
+    return {
+        type: BUY_ICECREAM,
+        info: 'Second Redux action'
+    }
+}
+
 //Reducer
 // (previousState, action) => newState
 
 const InitialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCream: 20
 }
-
+//having singular reducer is very diffcult to manage
 const reducer = (state = InitialState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
@@ -29,6 +38,10 @@ const reducer = (state = InitialState, action) => {
             // spread operator
             ...state,
             numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
         }
         //For managing default state we return back the state
         //One important thing to note down here that we are not mutating the
@@ -50,7 +63,11 @@ console.log('Initial Stat',  store.getState())
 const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()) )
 
 //Accepts action as a parameter.As paramter we will invoke(call) the action created
-store.dispatch(bycake)
-store.dispatch(bycake)
-store.dispatch(bycake)
+store.dispatch(bycake())
+store.dispatch(bycake())
+store.dispatch(bycake())
+
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
+store.dispatch(buyicecream())
 unsubscribe()
